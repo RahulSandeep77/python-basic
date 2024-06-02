b9ee8dbd1428d5d1535e13d91e69cb91e897be4e
RahulSandeep77 <deeparahulsandeep@gmail.com>
2024-06-02 14:45:51 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,10 @@
+# Simulate the traffic light
+traffic_light_colour = input()
+if(traffic_light_colour == "red"):
+    print("Stop! The light is red")
+elif(traffic_light_colour == "yellow"):
+        print("Slow down and stop")
+elif(traffic_light_colour == "green"):
+    print("Go! The light is green.")
+else:
+    print("enter traffic light color.")
